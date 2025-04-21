55a0d2e02d58fa22379dbd053d1f86cb869e260c
priyadixit123 <48387221+priyadixit123@users.noreply.github.com>
2025-04-21 12:11:10 +0530

    150. Evaluate Reverse Polish Notation.rs

---
+++ 150. Evaluate Reverse Polish Notation.rs
@@ -0,0 +1,38 @@
+https://leetcode.com/problems/evaluate-reverse-polish-notation/?envType=study-plan-v2&envId=top-interview-150
+
+Sol:
+impl Solution {
+    pub fn eval_rpn(tokens: Vec<String>) -> i32 {
+        let mut stack = Vec::new();
+
+        for token in tokens {
+            match token.as_str() {
+                "+" => {
+                    let b = stack.pop().unwrap();
+                    let a = stack.pop().unwrap();
+                    stack.push(a + b);
+                }
+                "-" => {
+                    let b = stack.pop().unwrap();
+                    let a = stack.pop().unwrap();
+                    stack.push(a - b);
+                }
+                "*" => {
+                    let b = stack.pop().unwrap();
+                    let a = stack.pop().unwrap();
+                    stack.push(a * b);
+                }
+                "/" => {
+                    let b = stack.pop().unwrap();
+                    let a = stack.pop().unwrap();
+                    stack.push(a / b); // Integer division truncates toward 0
+                }
+                num => {
+                    stack.push(num.parse::<i32>().unwrap());
+                }
+            }
+        }
+
+        stack[0]
+    }
+}
