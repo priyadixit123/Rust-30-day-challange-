4db6bed40159f3b86079f3cdfa960a860596fb63
priyadixit123 <48387221+priyadixit123@users.noreply.github.com>
2025-04-27 07:34:38 +0530

    3392. Count Subarrays of Length Three With a Condition.rs

--- 3392. Count Subarrays of Length Three With a Condition.rs
+++ 3392. Count Subarrays of Length Three With a Condition.rs
@@ -2,11 +2,14 @@ https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/d
 
 
 Sol:
-impl Solution {
+ impl Solution {
     pub fn count_subarrays(nums: Vec<i32>) -> i32 {
         let mut count = 0;
-        for i in 0..nums.len() - 2 {
-            if nums[i] + nums[i + 2] == nums[i + 1] / 2 {
+        for i in 0..nums.len().saturating_sub(2) {
+            let a = nums[i];
+            let b = nums[i + 1];
+            let c = nums[i + 2];
+            if (a + c) * 2 == b {
                 count += 1;
             }
         }
